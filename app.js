c5fb988b99ee008a0b2dd6d2328abda328b95ff1
taras <tarasivashchuk@gmail.com>
2022-06-23 23:20:30 +0300

    init graphql :  - added schema  - added resolvers  - added in app as middleware

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const path = require("path");
 const multer = require("multer");
+const {graphqlHTTP} = require("express-graphql");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
 
 const app = express();
 
@@ -19,14 +22,13 @@ const fileStorage = multer.diskStorage({
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
     cb(null, true);
-  }
-  else {
+  } else {
     cb(null, false);
   }
 };
 
 app.use(bodyParser.json());
-app.use(multer({ storage: fileStorage, fileFilter }).single("image"));
+app.use(multer({storage: fileStorage, fileFilter}).single("image"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
@@ -36,11 +38,17 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/graphql", graphqlHTTP({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+  graphiql: true
+}));
+
 app.use((err, req, res) => {
   const data = err.data;
   const status = err.statusCode || 500;
   const message = err.message;
-  res.status(status).json({ message, data });
+  res.status(status).json({message, data});
 });
 
 mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING)
