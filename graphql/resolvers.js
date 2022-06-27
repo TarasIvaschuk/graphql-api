fbba55b6c6c79f845d806518a8fff0c247f14ab4
taras <tarasivashchuk@gmail.com>
2022-06-28 00:37:01 +0300

    added createUser controller added query for testing

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,27 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
 module.exports = {
-  hello(){
+  createUser: async function ({userInput}, req) {
+    const existingUser = await User.findOne({email: userInput.email});
+    if (existingUser) {
+      throw  new Error("User exists already!");
+    }
+    const hashedPsw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPsw
+    });
+    const createdUser = await user.save();
     return {
-      text: 'Hello World!',
-      views: 12345
+      ...createdUser._doc, _id: createdUser._id.toString()
+    };
+  },
+  hello () {
+    return {
+      age: 37,
+      hobby: ['music','reading books','learning German']
     }
   }
-}
+};
